e40d8d2f1f4d158cab62700fc6f5a2e270a81725
Sofi.Maye <sofi.mann99@gmail.com>
2023-08-09 22:22:14 +0300

    finished ConfirmedBooking component and added styles

--- src/ConfirmedBooking.js
+++ src/ConfirmedBooking.js
@@ -1,8 +1,26 @@
 import React from "react";
+import "./ConfirmedBooking.css";
+
 export default function ConfirmedBooking() {
-    return(
-        <>
-            Your booking has been confirmed
-        </>
-    )
+    return (
+        <div className="confirmation-container">
+        <div className="confirmed-booking">
+            <svg
+                className="confirmation-image"
+                xmlns="http://www.w3.org/2000/svg"
+                width="100"
+                height="100"
+                viewBox="0 0 100 100"
+            >
+                <circle cx="50" cy="50" r="45" fill="#F4CE14" />
+                <path
+                    d="M28 49.9L42 63.9 73.3 32.5 68.7 28 42 54.7z"
+                    fill="#808080"
+                />
+            </svg>
+            <h2>Your Booking Has Been Confirmed</h2>
+            <p>Thank you for choosing us! We look forward to serving you.</p>
+        </div>
+        </div>
+    );
 }
